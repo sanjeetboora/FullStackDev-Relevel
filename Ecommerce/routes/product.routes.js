b8f897e9b394f1942086c7d0cbec8ef7fa57c68e
Sanjeet Boora <sanjeet.boora97@gmail.com>
2022-09-07 21:21:01 +0530

    product basic get, create, update, delete api added

--- Ecommerce/routes/product.routes.js
+++ Ecommerce/routes/product.routes.js
@@ -5,6 +5,18 @@ const routes = (app) => {
     /* to get all the products */
     app.get('/ecomm/api/v1/products', ProductController.getProducts);
 
+    /* to get all the products with Categories*/
+    app.get('/ecomm/api/v1/productsWithCategories', ProductController.getProductsWithCategories);
+
+    /* to create a product*/
+    app.post('/ecomm/api/v1/products', ProductController.createProduct);
+
+    /* to update a product*/
+    app.put('/ecomm/api/v1/products/:productId', ProductController.updateProduct);
+    
+    /* to delete a product*/
+    app.delete('/ecomm/api/v1/products/:productId', ProductController.deleteProduct);
+
 }
 
 module.exports = routes;
