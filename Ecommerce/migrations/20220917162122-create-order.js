8be78b0e23c1b9800d48812d11754874cc40e6ff
Sanjeet Boora <sanjeet.boora97@gmail.com>
2022-09-17 22:03:22 +0530

    orders model created

---
+++ Ecommerce/migrations/20220917162122-create-order.js
@@ -0,0 +1,31 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Orders', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      status: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      delivery: {
+        type: Sequelize.DATEONLY
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Orders');
+  }
+};
---
+++ Ecommerce/models/order.js
@@ -0,0 +1,24 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Order extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Order.init({
+    status: DataTypes.STRING,
+    delivery: DataTypes.DATEONLY
+  }, {
+    sequelize,
+    modelName: 'Order',
+  });
+  return Order;
+};
